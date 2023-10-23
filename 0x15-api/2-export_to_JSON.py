6ba4a9eae2c37216f43738c51f861baa144c1128
Diana Sungu <sungudiana1@gmail.com>
2023-10-23 23:21:20 +0300

    A script that fetches info about a given employee using an api and exports it in json format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Ascript that fetches data about an employee using an api
-and exports it in csv format
+"""Ascript that fetches info about an employee using an api
+and exports it in json format
 """
 import json
 import requests
@@ -24,23 +24,33 @@ if __name__ == "__main__":
     data = json.loads(data)
 
     user_name = data[0].get('username')
+    # print("id is: {}".format(user_id))
+    # print("username is: {}".format(user_name))
 
+    # get user info about todo tasks
+    # e.g https://jsonplaceholder.typicode.com/users/1/todos
     tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    # print("tasks url is: {}".format(tasks_url))
 
     response = requests.get(tasks_url)
 
     tasks = response.text
 
     tasks = json.loads(tasks)
+    # print("JSOON LOADS IS: {}".format(tasks))
 
-    # build the csv
-    builder = ""
+    dict_key = str(user_id)
+    # print("dict_key: {}".format(dict_key))
+
+    builder = {dict_key: []}
     for task in tasks:
-        builder += '"{}","{}","{}","{}"\n'.format(
-            user_id,
-            user_name,
-            task['completed'],
-            task['title']
-        )
-    with open('{}.csv'.format(user_id), 'w', encoding='UTF8') as myFile:
-        myFile.write(builder)
+        json_data = {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": user_name
+        }
+
+        builder[dict_key].append(json_data)
+    json_encoded_data = json.dumps(builder)
+    with open('{}.json'.format(user_id), 'w', encoding='UTF8') as myFile:
+        myFile.write(json_encoded_data)
